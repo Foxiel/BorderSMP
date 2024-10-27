ab6f06047e80eca03997ba5ab5b2dcc0b5f15433
Fabian <105858467+Foxiel@users.noreply.github.com>
2024-10-27 16:21:43 +0100

    Initial commit

---
+++ src/main/kotlin/org/bordersmp/borderSMP/BorderSMP.kt
@@ -0,0 +1,14 @@
+package org.bordersmp.borderSMP
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BorderSMP : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
